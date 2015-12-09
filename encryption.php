5c381e7e18c649e8fc4486c49cb94cd2999a53a5
Arunkumar Palaniappan <akrp@users.noreply.github.com>
2015-12-09 17:32:09 +0530

    Update encryption.php

--- encryption.php
+++ encryption.php
@@ -1,23 +1,24 @@
 <?php
-function encrypt($input)
+//function to encrypt a string
+function encrypt($input)//pass the string to the function 
 {
-    $key = pack('H*', "12345678901234567890123456789012");
-    $ivs = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-    $iv = mcrypt_create_iv($ivs, MCRYPT_RAND);
-    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key,$input, MCRYPT_MODE_CBC, $iv);
-    $ciphertext = $iv . $ciphertext;
-    $output= base64_encode($ciphertext);
-	return $output;
+    $key = pack('H*', "12345678901234567890123456789012");//change the key as you need
+    $ivs = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC); //get iv size
+    $iv = mcrypt_create_iv($ivs, MCRYPT_RAND);//create an iv random value
+    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key,$input, MCRYPT_MODE_CBC, $iv); //encrypt the string
+    $ciphertext = $iv . $ciphertext; //add with created iv value
+    $output= base64_encode($ciphertext); //change to base64 format
+    return $output;//return output
 }
-function decrypt($input)
+function decrypt($input)//pass the string to the function
 {
-    $key = pack('H*', "12345678901234567890123456789012"");
-    $ivs = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-    $iv = mcrypt_create_iv($ivs, MCRYPT_RAND);
-    $ciphertext_dec = base64_decode($input);
-    $iv_dec = substr($ciphertext_dec, 0, $ivs);
-    $ciphertext_dec = substr($ciphertext_dec, $ivs);
-    $output = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,$ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-	return $output;
+    $key = pack('H*', "12345678901234567890123456789012");//change the key as you need but same as the key used to encrypt
+    $ivs = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);//get iv size
+    $iv = mcrypt_create_iv($ivs, MCRYPT_RAND);//create an iv random value
+    $ciphertext_dec = base64_decode($input);//initially decode string from base64 format
+    $iv_dec = substr($ciphertext_dec, 0, $ivs);//split the iv random value from the string
+    $ciphertext_dec = substr($ciphertext_dec, $ivs); //split the iv random value from the string
+    $output = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,$ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec); //decrypt the string
+    return $output;//return output
 }
 ?>
