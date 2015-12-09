1609f8d3dc47681b53e340306b1369591e03ee78
Arunkumar Palaniappan <akrp@users.noreply.github.com>
2015-12-09 17:53:16 +0530

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,25 @@
+<?php
+include("https://raw.githubusercontent.com/akrp/phpencryption/master/encryption.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	$string = $_POST['string'];
+	$estring = encrypt($string);
+	$dstring = decrypt($estring);
+	
+}
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>PHP Encryption and Decryption</title>
+	</head>
+	<body>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			Enter a string to encrypt : <input type="text" name="string" required /><br>
+			<input type="submit" value="Encrypt" />
+		</form>
+	<?php echo $myinput ?><br>
+	<?php echo $einput ?><br>
+	<?php echo $dinput ?><br>
+	</body>
+</html>
